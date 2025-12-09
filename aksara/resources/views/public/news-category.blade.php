2468a100894b4a77da09fa4678428ea436f1336b
symphoen1x <someb350@gmail.com>
2025-12-09 13:35:29 +0700

    add

--- aksara/resources/views/public/news-category.blade.php
+++ aksara/resources/views/public/news-category.blade.php
@@ -146,10 +146,7 @@
                                         {{ strip_tags($berita->content) }}
                                     </p>
                                     <a href="{{ route('public.news', ['category' => $berita->category]) }}#{{ $berita->slug }}" class="inline-flex items-center text-indigo-600 font-semibold hover:translate-x-1 transition-transform">
-                                        Baca Selengkapnya
-                                        <svg class="w-4 h-4 ml-2" fill="none" stroke="currentColor" viewBox="0 0 24 24">
-                                            <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M17 8l4 4m0 0l-4 4m4-4H3"/>
-                                        </svg>
+                                  
                                     </a>
                                     <a href="{{ route('public.news.detail', ['category' => $berita->category, 'slug' => $berita->slug]) }}" class="inline-flex items-center text-indigo-600 font-semibold hover:translate-x-1 transition-transform">
                                         Baca Selengkapnya
